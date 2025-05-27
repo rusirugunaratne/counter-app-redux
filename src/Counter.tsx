78b198aa5dfcee0a6ce9fa9332af8c9d235143b4
rusirugunaratne <info.rusiru@gmail.com>
2025-05-27 22:06:36 +0530

    redux added

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,20 +1,11 @@
-import { useState } from "react"
 import "./Counter.css"
+import { useDispatch, useSelector } from "react-redux"
+import type { RootState } from "./state/store"
+import { decrement, incrementByAmount, reset } from "./state/counter/counterSlice"
 
 const Counter = () => {
-  const [count, setCount] = useState<number>(0)
-
-  const increment = (): void => {
-    setCount((prevCount) => prevCount + 1)
-  }
-
-  const decrement = (): void => {
-    setCount((prevCount) => prevCount - 1)
-  }
-
-  const reset = (): void => {
-    setCount(0)
-  }
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
 
   const getCountColor = (): string => {
     if (count > 0) return "positive"
@@ -30,13 +21,13 @@ const Counter = () => {
           <span className={`counter-value ${getCountColor()}`}>{count}</span>
         </div>
         <div className='counter-buttons'>
-          <button className='counter-btn decrement-btn' onClick={decrement} type='button'>
+          <button className='counter-btn decrement-btn' onClick={() => dispatch(decrement())} type='button'>
             -
           </button>
-          <button className='counter-btn reset-btn' onClick={reset} type='button'>
+          <button className='counter-btn reset-btn' onClick={() => dispatch(reset())} type='button'>
             Reset
           </button>
-          <button className='counter-btn increment-btn' onClick={increment} type='button'>
+          <button className='counter-btn increment-btn' onClick={() => dispatch(incrementByAmount(10))} type='button'>
             +
           </button>
         </div>
---
+++ src/state/counter/counterSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit"
+
+interface CounterState {
+  value: number
+}
+
+const initialState: CounterState = {
+  value: 0,
+}
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    reset: (state) => {
+      state.value = 0
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload
+    },
+  },
+})
+
+export default counterSlice.reducer
+
+export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions
